496af2ffd4ee9b3192a0e2111ba68c833d4002da
Fastering <73803835+Fastering18@users.noreply.github.com>
2021-01-20 12:14:49 +0700

    Update login.py

--- src/Discord/login.py
+++ src/Discord/login.py
@@ -2,10 +2,4 @@ import os
 from flask import Flask, g, session, redirect, request, url_for, jsonify
 from requests_oauthlib import OAuth2Session
 
-OAUTH2_CLIENT_ID = "798496088750292993"
-OAUTH2_CLIENT_SECRET = "aGcXd7_izART3A5zT-V6yhPqu-Fmr3-M"
-OAUTH2_REDIRECT_URI = 'http://localhost:5000/callback'
-
-API_BASE_URL = os.environ.get('API_BASE_URL', 'https://discordapp.com/api')
-AUTHORIZATION_BASE_URL = API_BASE_URL + '/oauth2/authorize'
-TOKEN_URL = API_BASE_URL + '/oauth2/token'
+# on working, available on our website
