4461567953f2ee028ffc035cdabfa8cf021f8268
Fastering <73803835+Fastering18@users.noreply.github.com>
2021-02-02 21:58:22 +0700

    Delete src directory

--- src/Discord/login.py
+++
@@ -1,6 +0,0 @@
-import os
-from flask import Flask, g, session, redirect, request, url_for, jsonify
-from requests_oauthlib import OAuth2Session
-
-
-# Source not available for public, please contact our developer for more info.
